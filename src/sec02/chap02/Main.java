305bb9b5aa30e9484270abfcb9ad0395944e35f1
hwajungchoi <fa4432@naver.com>
2023-07-21 11:10:54 +0900

    Changes

---
+++ src/sec02/chap02/Main.java
@@ -0,0 +1,14 @@
+package sec02.chap02;
+
+//        주석
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+       /* System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        여러 줄 주석
+        주석이 많이 필요하지 않은 코드가 좋은 코드이다. */
+//        System.out.println("Hello, World!"); command + /
+
+    }
+}
