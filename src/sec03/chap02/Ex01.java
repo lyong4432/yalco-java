9918d550f37e95ae454651a9baf97fa1fd3a7ffb
hwajungchoi <fa4432@naver.com>
2023-07-21 13:36:18 +0900

    Changes

---
+++ src/sec03/chap02/Ex01.java
@@ -0,0 +1,13 @@
+package sec03.chap02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        double a = 0.1, b = 0.2;
+
+        //  ⚠️ 오차가 생김
+        double c = a + b;
+        // float : 4바이트
+        // double : 8바이트
+        // double이 float보다 정밀하게 표현가능
+    }
+}
